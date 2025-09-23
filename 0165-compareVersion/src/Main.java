c92a5cc8c2a83ba86034603c159f5e0c0ea8f82f
alvaromsan <alvaro.marinsan@gmail.com>
2025-09-23 10:36:30 +0200

    Added 0165 challenge

---
+++ 0165-compareVersion/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        String v1 = "1.2";
+        String v2 = "1.10";
+
+        System.out.println("Result is: " + compareVersion(v1,v2));
+    }
+
+    public static int compareVersion(String version1, String version2) {
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
+
+        int maxLength = Math.max(v1.length, v2.length);
+
+        for(int i=0; i < maxLength; i++) {
+            int n1 = i < v1.length ? Integer.parseInt(v1[i]) : 0;
+            int n2 = i < v2.length ? Integer.parseInt(v2[i]) : 0;
+
+            if(n1 < n2) return -1;
+            else if (n1 > n2) return 1;
+        }
+
+        return 0; // equal
+    }
+}
